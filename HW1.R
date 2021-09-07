75b00f8d726c8aea9b1ec6687a625f9042535a12
Jonathan Eman <jonathaneman57@gmail.com>
2021-09-07 11:42:02 -0400

    Add files via upload

---
+++ HW1.R
@@ -0,0 +1,88 @@
+library(tidyverse)
+
+setwd("~/Fall 2021 Courses/STAT 4800")
+
+data <- read.csv("2019 PFF All Plays.csv")
+
+# Create empirical expected points model
+data %>%
+  mutate(
+    # Re-configure field position - (0 to 100 where 100 is touchdown)
+    FIELDPOSITION = case_when(
+      pff_FIELDPOSITION < 0 ~ pff_FIELDPOSITION * -1,
+      pff_FIELDPOSITION > 0 ~ 100 - pff_FIELDPOSITION
+    ),
+    # Create distance (YTG for first down) bins
+    DISTANCE_BIN = case_when(
+      pff_DISTANCE >= 8 ~ "Long",
+      between(pff_DISTANCE, 5, 7) ~ "Med",
+      between(pff_DISTANCE, 3, 4) ~ "Short",
+      pff_DISTANCE < 3 ~ "Shortest"
+    ),
+    # Create field position bins
+    FIELDPOSITION_BIN = case_when(
+      between(FIELDPOSITION, 0, 20) ~ "Own Side - Far",
+      between(FIELDPOSITION, 21, 50) ~ "Own Side",
+      between(FIELDPOSITION, 51, 79) ~ "Opponent Side",
+      between(FIELDPOSITION, 80, 89) ~ "Redzone - Far",
+      FIELDPOSITION >= 90 ~ "Redzone - Close"
+    ),
+    # Calculate change in score
+    POINTS_SCORED = case_when(
+      # pff_DRIVEPLAY == pff_DRIVEENDPLAYNUMBER ~ lead(pff_DEFSCORE) - pff_OFFSCORE,
+      # TRUE ~ lead(pff_OFFSCORE) - pff_OFFSCORE
+      (pff_DRIVEPLAY == pff_DRIVEENDPLAYNUMBER) & (pff_DRIVEENDEVENT == "FIELD GOAL") ~ 3,
+      (pff_DRIVEPLAY == pff_DRIVEENDPLAYNUMBER) & (pff_DRIVEENDEVENT == "FUMBLE-TD") ~ -6,
+      (pff_DRIVEPLAY == pff_DRIVEENDPLAYNUMBER) & (pff_DRIVEENDEVENT == "INTERCEPTION-TD") ~ -6,
+      (pff_DRIVEPLAY == pff_DRIVEENDPLAYNUMBER) & (pff_DRIVEENDEVENT == "SAFETY") ~ -2,
+      (pff_DRIVEPLAY == pff_DRIVEENDPLAYNUMBER) & (pff_DRIVEENDEVENT == "TOUCHDOWN") ~ 6,
+      TRUE ~ 0
+  )) %>% 
+  filter(
+    # Remove garbage time, lingering drives, and 2/4 quarters
+    pff_GARBAGETIME == 0,
+    pff_QUARTER %in% c(1,3),
+    # Remove no-score scenarios
+    pff_DRIVEENDEVENT %in% c("FIELD GOAL", "FUMBLE-TD", "INTERCEPTION-TD",
+                             "SAFETY", "TOUCHDOWN")
+  ) %>%
+  select(pff_QUARTER, FIELDPOSITION, FIELDPOSITION_BIN, pff_DISTANCE, DISTANCE_BIN,
+         pff_DRIVEPLAY, pff_DRIVEENDPLAYNUMBER, pff_DRIVEENDEVENT, pff_DOWN,
+         POINTS_SCORED) %>%
+  group_by(pff_DOWN, FIELDPOSITION_BIN, DISTANCE_BIN) %>%
+  summarize(expected_points = mean(POINTS_SCORED)) -> empirical_model
+
+### Write function to return expected points for given situation
+expected_points <- function(down, fp, ytg, pos) {
+    
+    fp_converted <- ifelse(
+      tolower(pos) == "own", fp, 100 - fp
+    )
+    # print(fp_converted)
+  
+    fp_bin <- case_when(
+      between(fp_converted, 0, 20) ~ "Own Side - Far",
+      between(fp_converted, 21, 50) ~ "Own Side",
+      between(fp_converted, 51, 79) ~ "Opponent Side",
+      between(fp_converted, 80, 89) ~ "Redzone - Far",
+      fp_converted >= 90 ~ "Redzone - Close"
+    )
+    # print(fp_bin)
+    
+    ytg_bin = case_when(
+      ytg >= 8 ~ "Long",
+      between(ytg, 5, 7) ~ "Med",
+      between(ytg, 3, 4) ~ "Short",
+      ytg < 3 ~ "Shortest"
+    )
+    # print(ytg_bin)
+    
+    empirical_model %>%
+      filter(
+        pff_DOWN == down,
+        FIELDPOSITION_BIN == fp_bin,
+        DISTANCE_BIN == ytg_bin
+      ) %>%
+      pull(expected_points)
+  }
+  
