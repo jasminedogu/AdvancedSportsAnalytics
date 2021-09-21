956cb7b833b0de7af1cc2c3e5a8300f2691bfd5e
jonathan-eman <jonathaneman57@gmail.com>
2021-09-20 22:03:30 -0400

    We did it

--- HW2.R
+++ HW2.R
@@ -1,5 +1,7 @@
 setwd("~/Fall 2021 Courses/STAT 4800")
 library(tidyverse)
+library(Rcpp)
+library(markovchain)
 
 data <- read.csv("SB_box_scores_2019_without_rank.csv") %>%
   mutate(point_diff = Pts_winner - Pts_loser)
@@ -9,6 +11,36 @@ data %>% select(data=Loser) %>% as.vector() -> Loser
 
 rbind(Winner, Loser) %>% unique() %>% pull() -> teams_list
 
-M1 <- matrix(0 ,nrow=217, ncol = 217) 
-rownames(M1) <- teams_list
-colnames(M1) <- teams_list
+team_matrix <- matrix(0 ,nrow=217, ncol = 217) 
+rownames(team_matrix) <- teams_list
+colnames(team_matrix) <- teams_list
+
+for (i in 1:nrow(data)) {
+  winner <- data$Winner[i]
+  loser <- data$Loser[i]
+  point_diff <- data$point_diff[i]
+  
+  win_prob <- case_when(
+    point_diff <= 7 ~ .6,
+    between(point_diff, 8, 14) ~ .7,
+    between(point_diff, 15, 21) ~ .8,
+    point_diff >= 21 ~ .9
+  )
+  
+  team_matrix[loser, winner] <- win_prob
+  team_matrix[winner, loser] <- (1-win_prob)
+  
+}
+
+for (i in 1:nrow(team_matrix)) {
+  
+  total <- sum(team_matrix[i,]) / 13
+  team_matrix[i,i] <- (1-total)
+  
+}
+
+team_matrix[upper.tri(team_matrix) | lower.tri(team_matrix)] <- team_matrix[upper.tri(team_matrix) | lower.tri(team_matrix)] / 13
+
+markov_chain <- new("markovchain", states = teams_list, transitionMatrix = team_matrix)
+
+steadyStates(markov_chain) %>% as.data.frame() %>% t() -> rankings
