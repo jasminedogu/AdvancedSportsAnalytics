a59f8e38423b75e9aaeacceed233137cafc5448e
jonathan-eman <jonathaneman57@gmail.com>
2021-09-28 12:11:11 -0400

    Start final project

---
+++ final_project_basic_drive.R
@@ -0,0 +1,85 @@
+play <- function() {
+  rnorm(1, 3.5, 1)
+}
+
+### posession function
+
+full_drive <- function(A, C, D){
+  
+  # A is starting field position of play (ranges from 0 to 100 (so your own 25 would be 25. 100 would be a touchdown))
+  # C is yards to first down
+  # D is down number
+  
+  is_not_done <- TRUE
+  k <- 0 #this allows us to gracefully kick out of a bad while loop. Not needed in production
+  #but in case you miscode something while running a while loop, it's always good to not have to kill the kernel.
+  
+  drive_result <- list(event = NA, score = NA, end_yard = NA)
+  
+  while (is_not_done == TRUE) {
+    
+    yards_gained <- play()
+        
+        A = A + yards_gained
+        C = C - yards_gained
+        D = ifelse(C <= 0, 1, D + 1)
+        
+        if (D == 1) { #if first down, reset YTG to 10
+          C = 10
+        }
+        
+        k <- k + 1
+        
+      if (D == 4) {  
+        if (A >= 70) { #if within 30 yards of end zone, simulate field goal
+          prob <- field_goal_probability(A)
+          field_goal <- sample.int(2, size = 1, prob = c(prob, 1 - prob))
+          if (field_goal == 1) {
+            drive_result$score <- 3
+            drive_result$event <- "FG"
+          } else {
+            drive_result$end_yard <- A
+            drive_result$event <- "Missed FG"
+          }
+        } else { #or else simulate punt
+          punt_yards <- rnorm(1, 40.5, 9.7)
+          drive_result$end_yard <- ifelse(A + punt_yards <= 0,
+                                          25,
+                                          A + punt_yards)
+          drive_result$event <- "Punt"
+        }
+          is_not_done <- FALSE
+        }
+        
+        # if (D == 5) { # turnover on downs
+        #   drive_result$end_yard <- A
+        #   drive_result$end_time <- B
+        #   drive_result$event <- "Turnover on Downs"
+        #   print("TOD")
+        #   is_not_done <- FALSE
+        # } 
+        # 
+        # if (D == 0) { # interception
+        #   drive_result$end_yard <- A
+        #   drive_result$end_time <- B
+        #   drive_result$event <- "Interception"
+        #   print("Interception")
+        #   is_not_done <- FALSE
+        # }
+        
+     if (A >= 100) {
+        drive_result$score <- 7
+        drive_result$event <- "Touchdown"
+        print("Touchdown")
+        is_not_done <- FALSE
+      }
+      
+      if(k > 100){
+        drive_result$end_yard <- A
+        is_not_done <- FALSE
+      }
+        
+  }
+  
+  print(drive_result)
+}
