776f2b6a28ff923797a474d498e96b4275ee9661
jonathan-eman <jonathaneman57@gmail.com>
2021-12-03 09:58:24 -0500

    Updates

--- expected_points.R
+++ expected_points.R
@@ -55,10 +55,108 @@ monte_carlo <- replicate(10000,
 mean(monte_carlo) -> ep_95_100
 
 # E[1st & 10 at FP+YTG] (after we convert on 4th down)
+# 60-68 FP, 0-1 YTG
+replicate(10000,
+          score_simulator(fp = sample(60:68, 1) + 1,
+                          ytg = 10,
+                          down = 1,
+                          pos = 0
+          )) %>% mean()
+
 # 60-68 FP, 2-3 YTG
 replicate(10000,
           score_simulator(fp = sample(60:68, 1) + sample(2:3, 1),
                           ytg = 10,
                           down = 1,
                           pos = 0
+          )) %>% mean()
+
+
+# 60-68 FP, 4-5 YTG
+replicate(10000,
+          score_simulator(fp = sample(60:68, 1) + sample(4:5, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 0
+          )) %>% mean()
+
+# 60-68 FP, 6-7 YTG
+replicate(10000,
+          score_simulator(fp = sample(60:68, 1) + sample(4:5, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 0
+          )) %>% mean()
+
+# 60-68 FP, 8-10 YTG
+replicate(10000,
+          score_simulator(fp = sample(60:68, 1) + sample(4:5, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 0
+          )) %>% mean()
+
+# 95-100 FP, 0-1 YTG
+replicate(10000,
+          score_simulator(fp = sample(95:100, 1) + sample(0:1, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 0
+          )) %>% mean()
+
+# 95-100 FP, 2-3 YTG
+replicate(10000,
+          score_simulator(fp = sample(95:100, 1) + sample(2:3, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 0
+          )) %>% mean()
+
+# 95-100 FP, 4-5 YTG
+replicate(10000,
+          score_simulator(fp = sample(95:100, 1) + sample(4:5, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 0
+          )) %>% mean()
+
+# E[Opp 1st & 10 at FP] (after we convert on 4th down)
+# 60-68 FP
+replicate(10000,
+          score_simulator(fp = 100-sample(60:68, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 1
+          )) %>% mean()
+
+# 69-77 FP
+replicate(10000,
+          score_simulator(fp = 100-sample(69:77, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 1
+          )) %>% mean()
+
+# 78-86 FP
+replicate(10000,
+          score_simulator(fp = 100-sample(78:86, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 1
+          )) %>% mean()
+
+# 87-94 FP
+replicate(10000,
+          score_simulator(fp = 100-sample(87:94, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 1
+          )) %>% mean()
+
+# 95-100 FP
+replicate(10000,
+          score_simulator(fp = 100-sample(95:100, 1),
+                          ytg = 10,
+                          down = 1,
+                          pos = 1
           )) %>% mean()
